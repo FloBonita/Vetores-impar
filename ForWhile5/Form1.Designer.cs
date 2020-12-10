7d7dcd9cf20deffebc743b63e4b9090bafbb432d
FloBonita <74161621+FloBonita@users.noreply.github.com>
2020-12-10 16:58:38 -0300

    Estudo sobre C#.

---
+++ ForWhile5/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ForWhile5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lst1 = new System.Windows.Forms.ListBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lbl1.Location = new System.Drawing.Point(12, 26);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(310, 24);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Insira o Primeiro número:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lbl2.Location = new System.Drawing.Point(12, 77);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(298, 24);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Insira o Segundo número:";
+            // 
+            // txt1
+            // 
+            this.txt1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.txt1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.ForeColor = System.Drawing.Color.White;
+            this.txt1.Location = new System.Drawing.Point(328, 19);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(97, 37);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.txt2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.ForeColor = System.Drawing.Color.White;
+            this.txt2.Location = new System.Drawing.Point(328, 70);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(97, 37);
+            this.txt2.TabIndex = 4;
+            // 
+            // lst1
+            // 
+            this.lst1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lst1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst1.ForeColor = System.Drawing.Color.White;
+            this.lst1.FormattingEnabled = true;
+            this.lst1.ItemHeight = 29;
+            this.lst1.Location = new System.Drawing.Point(16, 159);
+            this.lst1.Name = "lst1";
+            this.lst1.Size = new System.Drawing.Size(409, 149);
+            this.lst1.TabIndex = 5;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn1.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Location = new System.Drawing.Point(117, 314);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 33);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "VAI";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn2.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(224, 314);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(98, 33);
+            this.btn2.TabIndex = 9;
+            this.btn2.Text = "LIMPA";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.White;
+            this.lbl3.Location = new System.Drawing.Point(46, 127);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 29);
+            this.lbl3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(473, 355);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lst1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Exercicio 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.ListBox lst1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
+
---
+++ ForWhile5/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForWhile5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int N1, N2, i;
+            N1 = Convert.ToInt32(txt1.Text);
+            N2 = Convert.ToInt32(txt2.Text);
+            i = N1;
+
+            if (N1 == N2)
+            {
+                MessageBox.Show("Os números são iguais, o primeiro número deve ser menor que o segundo");
+            }           
+            while (N1 < N2)
+            {
+                lbl3.Text=("Os numeros impares entre " + i + " & " + N2 + " são: ");
+                if (N1 % 2 != 0)
+                {
+                  lst1.Items.Add(N1);
+                }
+                N1++;                   
+            }                
+            while (N2<N1)
+            {
+              lbl3.Text = ("Os numeros pares entre " + i + " & " + N2 + " são: ");
+              if (N2 % 2 == 0)
+              {
+                lst1.Items.Add(N2);
+              }
+              N2++;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lst1.Items.Clear();
+            lbl3.Text="";
+            txt1.Text = (" ");
+            txt2.Text = (" ");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+    
+        
+    
+
